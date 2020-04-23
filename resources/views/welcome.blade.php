bd94c08fc1ac913b9594fc29544931fefd5fdb9c
soy-software <soysoftware@gmail.com>
2020-04-23 11:47:58 -0500

    welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div style="height: 75vh">
     <div class="flex-center flex-column">
-      <h5 class="animated fadeIn mb-3">Bienvenido {{ config('app.name','Clinica-UTC') }}</h5>
+      <h5 class="animated fadeIn mb-3">Bienvenido <strong>{{ config('app.name','Clinica-UTC') }}</strong></h5>
       <p class="animated fadeIn text-muted">{{ date('Y') }}</p>
     </div>
   </div>
